18f3a9f039e0d14c9c721d7deecc780369f939fa
LogicalError <75528671+NCH-dev@users.noreply.github.com>
2023-11-22 18:47:28 +0200

    Delete acmonitor.h

--- acmonitor.h
+++
@@ -1,8 +0,0 @@
-
-#ifndef ACMONITOR_H // Include guard to prevent multiple inclusion
-#define ACMONITOR_H
-
-int findMal(unsigned char* path);
-int file_modifications(unsigned char* path);
-
-#endif
